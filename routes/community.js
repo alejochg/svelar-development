1f166687a1ebc6451713e674ce3c8ee3dc8ba386
ibcd93 <raja4nagappan@gmail.com>
2017-07-25 14:46:45 -0700

    community page edits 1

--- routes/community.js
+++ routes/community.js
@@ -5,8 +5,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+/*router.get('/', function(req, res, next) {
+   res.render('community', {});
+}); */
+
+
 router.get('/', function(req, res, next) {
-    res.render('community', {});
+    if(req.user!=undefined) { // check if user has been authenticated
+        res.render('community', {title: 'Community'}); // If yes give access to profile
+    }else{
+        res.redirect('/users/login'); // If not redirect to login page
+    }
 });
 
 
